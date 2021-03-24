08272295324d2555af8a82fbc3d0631e1e09d759
irisdev06 <ivgb_@outlook.com>
2021-03-24 16:52:08 -0500

    Variables2

---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Iris"; 
+
+//Declarando
+
+var edad; 
+
+//Inicializar
+
+var edad;
+edad = 19;
+
+var elementos = ["Computador", "Celular"];
+
+//Variable tipo objeto
+
+var persona = {
+    nombre: "Iris",
+    edad: 19
+}
